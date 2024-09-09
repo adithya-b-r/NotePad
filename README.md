6ba47544821b6c8c28581e305db1afca2ee1b75b
Abr009 <bradithya945@gmail.com>
2024-09-09 21:07:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Note Pad
+
+![Note Pad](./public/images/initial.png)
+
+A simple web-based note-taking app built with Node.js, Express, EJS, and TailwindCSS. This app allows users to create, read, edit, and manage text files as notes.
+
+## Features
+
+- Create tasks and notes
+- View notes
+- Edit tasks and notes
+- Simple and clean user interface
+
+## Screenshots
+
+### Index Page
+![Index Page](./public/images/index.png)
+
+### Show File Page
+![Show File Page](./public/images/view.png)
+
+### Edit File Page
+![Edit File Page](./public/images/edit.png)
+
+## Tech Stack
+
+- **Backend**: Node.js, Express.js
+- **Frontend**: EJS, TailwindCSS
+- **Storage**: File System (using `fs` module)
+
+## How to Run the Project
+
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/yourusername/notepad.git
+    cd notepad
+    ```
+
+2. Install dependencies:
+    ```bash
+    npm install
+    ```
+
+3. Start the server:
+    ```bash
+    npm start
+    ```
+
+4. Open the app in your browser:
+    - Visit `http://localhost:3000`
--- index.js
+++ index.js
@@ -17,7 +17,44 @@ app.get('/', function (req, res) {
 })
 
 app.post('/create', function(req, res){
-  fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+  fs.writeFile(`./files/${req.body.title.split(' ').join('').replace('.txt','')}.txt`, req.body.details, function(err){
+    res.redirect("/")
+  })
+})
+
+app.get('/file/:filename', function(req, res){
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, fileData){
+    if(err){
+      console.error("File Not Found : "+err)
+    }else{
+      res.render("show", {fileName: req.params.filename, fileContent: fileData})
+    }
+  })
+})
+
+app.get('/edit/:filename', function(req, res){
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, fileData){
+    if(err){
+      console.error("File Not Found : "+err)
+    }else{
+      res.render("edit", {fileName: req.params.filename, fileContent: fileData})
+    }
+  })
+})
+function deleteFile(filePath){
+  fs.unlink(filePath, function (err) {
+    if (err)
+      console.error("Error : " + err)
+  })
+}
+
+app.post('/editFile', function(req, res){
+
+  newFile = `./files/${req.body.title.split(' ').join('').replace('.txt','')}.txt`
+
+  deleteFile(`./files/${req.body.previousName}`)
+
+  fs.writeFile(newFile, req.body.details, function(err){
     res.redirect("/")
   })
 })
